ae7e18d8959d9a1807c176653432bdcf98ba8254
Kris <isaevachristin@gmail.com>
2024-03-10 17:34:45 +0300

    добавила папки

---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+Console.Clear();
+int m = 0;
+int n = 1;
+int Akcerman(int m, int n)
+{
+  if (m == 0)
+    {
+      return n + 1;
+      }
+  else if (m > 0 && n == 0)
+    {
+      return Akcerman(m - 1, 1);
+      }
+  else if (m>0 && n> 0)
+    {
+      return Akcerman(m - 1, Akcerman(m, n - 1));
+      }
+  return -1;
+}
+Console.Write(Akcerman(m, n));
---
+++ 3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+//Использовать рекурсию, не использовать циклы.
+Console.Clear();
+int[]array = [1, 2, 5, 0, 10, 34];
+int size= array.Length;
+void ArrayRevers (int[]array, int size) 
+{
+    if (size==0)
+    {
+        return;
+    }
+
+Console.Write($"{array[size-1]} ");
+ArrayRevers(array,size-1);
+}
+ArrayRevers(array,size);
